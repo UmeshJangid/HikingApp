fd9fef7c6ba02b22f773875bc4566fc3ca70ce43
Umesh Jangid <umesh.jangid@spardha.co>
2024-07-28 14:41:17 +0530

    Initial Commit

---
+++ HikingApp/HikingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingAppApp.swift
+//  HikingApp
+//
+//  Created by Umesh Jangid on 28/07/24.
+//
+
+import SwiftUI
+
+@main
+struct HikingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
